36e2ea8ccf65069df9b4fda3431bb4d5e3ef423b
jsj <jsj>
2024-12-21 15:02:37 +0100

    fix pytest

--- src/msfabricutils/etl/load/__init__.py
+++ src/msfabricutils/etl/load/__init__.py
@@ -1,3 +1,3 @@
-from .delta_table import append, incremental, overwrite, replace_range, upsert, write
+from .delta_table import append, incremental, overwrite, replace_range, upsert
 
 __all__ = ("write", "upsert", "overwrite", "append", "incremental", "replace_range")
