963629cecd54c22b13296f650fb43aab47eda803
jsj <jsj>
2024-12-22 18:04:11 +0100

    better requests

--- src/msfabricutils/core/fabric_request.py
+++ src/msfabricutils/core/fabric_request.py
@@ -1,9 +1,11 @@
+import logging
+
 import requests
 
 from msfabricutils.core.auth import get_fabric_bearer_token
 
 
-def paginated_get_request(endpoint: str, data_key: str) -> list[dict]:
+def paginated_get_request(endpoint: str, data_key: str) -> list[dict[str, str]]:
     """
     Retrieves paginated data from the specified API endpoint.
 
@@ -44,7 +46,7 @@ def paginated_get_request(endpoint: str, data_key: str) -> list[dict]:
     return responses
 
 
-def get_item_from_paginated_get_request(endpoint: str, data_key: str, item_key: str, item_value: str) -> list[dict]:
+def get_item_from_paginated_get_request(endpoint: str, data_key: str, item_key: str, item_value: str) -> dict[str, str]:
     """
     Recursively paginates the API endpoint until specified item is found and returns it.
 
@@ -90,7 +92,7 @@ def get_item_from_paginated_get_request(endpoint: str, data_key: str, item_key: 
     raise ValueError(f"Item with {item_key} {item_value} not found")
 
 
-def get_request(endpoint: str) -> list[dict]:
+def get_request(endpoint: str, content_only: bool = True) -> requests.Response | dict[str, str]:
     """
     Retrieves data from a specified API endpoint.
 
@@ -100,9 +102,10 @@ def get_request(endpoint: str) -> list[dict]:
 
     Args:
         endpoint (str): The API endpoint to send the GET request to.
-
+        content_only (bool): Whether to return the content of the response only.
+    
     Returns:
-        A list of dictionaries containing the data returned from the API.
+        A list of dictionaries containing the data returned from the API or the response object.
 
     Raises:
         requests.exceptions.RequestException: If the HTTP request fails or returns an error.
@@ -113,37 +116,106 @@ def get_request(endpoint: str) -> list[dict]:
     params = {}
 
     response = requests.get(f"{base_url}/{endpoint}", headers=headers, params=params)
-    response.raise_for_status()
 
-    return response.json()
+    if content_only:
+        if response.status_code >= 400:
+            logging.error(f"Request failed with status code {response.status_code}: {response.json()}")
+        response.raise_for_status()
+        return response.json()
+
+    return response
 
 
-def post_request(endpoint: str, data: dict[str, str]) -> dict[str, str]:
+def post_request(endpoint: str, data: dict[str, str], content_only: bool = True) -> requests.Response | dict[str, str]:
+    """
+    Sends a POST request to a specified API endpoint.
+
+    This function makes a POST request to the specified endpoint of the Azure Fabric API,
+    using a bearer token for authentication. It sends the provided data in JSON format
+    and returns either the JSON response or the full response object.
+
+    Args:
+        endpoint (str): The API endpoint to send the POST request to.
+        data (dict[str, str]): The data to be sent in the request body.
+        content_only (bool): Whether to return the content of the response only.
+
+    Returns:
+        Either the JSON response as a dictionary or the full response object.
+
+    Raises:
+        requests.exceptions.RequestException: If the HTTP request fails or returns an error.
+    """
     base_url = "https://api.fabric.microsoft.com/v1"
     token = get_fabric_bearer_token()
     headers = {"Authorization": f"Bearer {token}"}
 
     response = requests.post(f"{base_url}/{endpoint}", headers=headers, json=data)
-    response.raise_for_status()
 
-    return response.json()
+    if content_only:
+        if response.status_code >= 400:
+            logging.error(f"Request failed with status code {response.status_code}: {response.json()}")
+        response.raise_for_status()
+        return response.json()
+
+    return response
 
 
-def patch_request(endpoint: str, data: dict[str, str]) -> dict[str, str]:
+def patch_request(endpoint: str, data: dict[str, str], content_only: bool = True) -> requests.Response | dict[str, str]:
+    """
+    Sends a PATCH request to a specified API endpoint.
+
+    This function makes a PATCH request to the specified endpoint of the Azure Fabric API,
+    using a bearer token for authentication. It sends the provided data in JSON format
+    and returns either the JSON response or the full response object.
+
+    Args:
+        endpoint (str): The API endpoint to send the PATCH request to.
+        data (dict[str, str]): The data to be sent in the request body.
+        content_only (bool): Whether to return the content of the response only.
+
+    Returns:
+        Either the JSON response as a dictionary or the full response object.
+
+    Raises:
+        requests.exceptions.RequestException: If the HTTP request fails or returns an error.
+    """
     base_url = "https://api.fabric.microsoft.com/v1"
     token = get_fabric_bearer_token()
     headers = {"Authorization": f"Bearer {token}"}
 
     response = requests.patch(f"{base_url}/{endpoint}", headers=headers, json=data)
-    response.raise_for_status()
 
-    return response.json()
+    if content_only:
+        if response.status_code >= 400:
+            logging.error(f"Request failed with status code {response.status_code}: {response.json()}")
+        response.raise_for_status()
+        return response.json()
+
+    return response
 
 
-def delete_request(endpoint: str) -> dict[str, str]:
+def delete_request(endpoint: str) -> requests.Response:
+    """
+    Sends a DELETE request to a specified API endpoint.
+
+    This function makes a DELETE request to the specified endpoint of the Azure Fabric API,
+    using a bearer token for authentication.
+
+    Args:
+        endpoint (str): The API endpoint to send the DELETE request to.
+
+    Returns:
+        The response object from the DELETE request.
+
+    Raises:
+        requests.exceptions.RequestException: If the HTTP request fails or returns an error.
+    """
     base_url = "https://api.fabric.microsoft.com/v1"
     token = get_fabric_bearer_token()
     headers = {"Authorization": f"Bearer {token}"}
 
     response = requests.delete(f"{base_url}/{endpoint}", headers=headers)
+    if response.status_code >= 400:
+        logging.error(f"Request failed with status code {response.status_code}: {response.json()}")
     response.raise_for_status()
+    return response
