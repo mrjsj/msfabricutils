bad3e07514a337372a7f63fff97248b3a929a217
jsj <jsj>
2024-12-23 14:27:17 +0100

    expose in top level

--- src/msfabricutils/__init__.py
+++ src/msfabricutils/__init__.py
@@ -4,6 +4,7 @@ from msfabricutils.common.fabric_duckdb_connection import FabricDuckDBConnection
 from msfabricutils.core import (
     get_azure_storage_access_token,
     get_fabric_bearer_token,
+    get_onelake_access_token,
     get_workspace,
     get_workspace_lakehouse_tables,
     get_workspace_lakehouses,
@@ -14,6 +15,7 @@ __all__ = (
     "FabricDuckDBConnection",
     "get_fabric_bearer_token",
     "get_azure_storage_access_token",
+    "get_onelake_access_token",
     "get_workspace",
     "get_workspace_lakehouse_tables",
     "get_workspace_lakehouses",
